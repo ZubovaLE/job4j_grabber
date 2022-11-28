aa584e9ae006230b0051194f12694fe5d3d389fd
NikiforovaLE <n.lubovj@gmail.com>
2022-11-28 23:10:09 +0300

    Correct AlertRabbit class

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,18 +3,22 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(readSettings())
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -26,9 +30,19 @@ public class AlertRabbit {
         }
     }
 
+    private static int readSettings() throws IOException, NumberFormatException {
+        Properties config = new Properties();
+        int seconds;
+        try (InputStream is = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            config.load(is);
+            seconds = Integer.parseInt(config.getProperty("rabbit.interval"));
+        }
+        return seconds;
+    }
+
     public static class Rabbit implements Job {
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
         }
     }
