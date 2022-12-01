154b32f19041aabe16014f32cf5c5d268401f71d
NikiforovaLE <n.lubovj@gmail.com>
2022-12-01 23:52:42 +0300

    Add new dependency in pom.xml file. Improve AlertRabbit class

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,11 +5,8 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -17,19 +14,43 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+    private static Properties getProperties() {
+        Properties config = new Properties();
+        try (InputStream is = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            config.load(is);
+            return config;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return config;
+    }
+
+    private static int readInterval(Properties properties) throws NumberFormatException {
+        return Integer.parseInt(properties.getProperty("rabbit.interval"));
+    }
+
+    private static Connection getConnection(Properties properties) throws IOException, ClassNotFoundException, SQLException {
+        Class.forName(properties.getProperty("driver"));
+        return DriverManager.getConnection(
+                properties.getProperty("url"),
+                properties.getProperty("username"),
+                properties.getProperty("password")
+        );
+    }
+
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
-        try (Connection connection = getConnection()) {
+        Properties properties = getProperties();
+        try (Connection connection = getConnection(properties)) {
             try {
-                List<Long> store = new ArrayList<>();
                 Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
                 scheduler.start();
                 JobDataMap data = new JobDataMap();
-                data.put("store", store);
+                data.put("DB", connection);
                 JobDetail job = newJob(Rabbit.class)
                         .usingJobData(data)
                         .build();
                 SimpleScheduleBuilder times = simpleSchedule()
-                        .withIntervalInSeconds(readSettings())
+                        .withIntervalInSeconds(readInterval(properties))
                         .repeatForever();
                 Trigger trigger = newTrigger()
                         .startNow()
@@ -38,46 +59,35 @@ public class AlertRabbit {
                 scheduler.scheduleJob(job, trigger);
                 Thread.sleep(10000);
                 scheduler.shutdown();
-                System.out.println(store);
+                try (Statement statement = connection.createStatement()) {
+                    ResultSet selection = statement.executeQuery("SELECT * FROM rabbit");
+                    var metaData = selection.getMetaData();
+                    for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                        System.out.println(metaData.getColumnName(i) + ", " + metaData.getColumnTypeName(i));
+                    }
+                }
             } catch (Exception se) {
                 se.printStackTrace();
             }
         }
     }
 
-    private static Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
-        try (InputStream is = Rabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-            Properties properties = new Properties();
-            properties.load(is);
-            Class.forName(properties.getProperty("driver"));
-            return DriverManager.getConnection(
-                    properties.getProperty("url"),
-                    properties.getProperty("username"),
-                    properties.getProperty("password")
-            );
-        }
-    }
-
-    private static int readSettings() throws IOException, NumberFormatException {
-        Properties config = new Properties();
-        int seconds;
-        try (InputStream is = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-            config.load(is);
-            seconds = Integer.parseInt(config.getProperty("rabbit.interval"));
-        }
-        return seconds;
-    }
-
     public static class Rabbit implements Job {
-        public Rabbit() throws SQLException, IOException, ClassNotFoundException {
+        public Rabbit() {
             System.out.println(hashCode());
         }
 
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
-            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
-            store.add(System.currentTimeMillis());
+            try (Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("DB")) {
+                try (PreparedStatement statement = connection.prepareStatement("INSERT INTO rabbit(created_data) VALUES(?)")) {
+                    statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().withNano(0)));
+                    statement.executeUpdate();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
