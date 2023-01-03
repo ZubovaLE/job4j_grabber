9dda9a02bfedd99c34e2056e685526ad8c878579
NikiforovaLE <n.lubovj@gmail.com>
2023-01-03 22:48:39 +0300

    Correct control_queries.sql

--- src/main/java/ru/job4j/exam/control_queries.sql
+++ src/main/java/ru/job4j/exam/control_queries.sql
@@ -1,22 +1,31 @@
-CREATE TABLE company(
-    id INTEGER NOT NULL,
+create table company(
+    id integer not null,
     name CHARACTER VARYING,
-    CONSTRAINT company_pkey PRIMARY KEY (id)
+    constraint company_pkey primary key (id)
 );
 
-CREATE TABLE person(
-    id INTEGER NOT NULL,
+create table person(
+    id integer not null,
     name CHARACTER VARYING,
-    company_id INTEGER REFERENCES company(id),
-    CONSTRAINT person_pkey PRIMARY KEY (id)
+    company_id integer references company(id),
+    constraint person_pkey primary key (id)
 );
 
-INSERT INTO company (id, name) VALUES (1, 'Company 1'), (2, 'Company 2'), (3, 'Company 3'), (4,'Company 4'), (5,'Company 5');
-INSERT INTO person (id, name, company_id) VALUES (1, 'Person 1', 1), (2, 'Person 2', 2), (3, 'Person 3', 3), (4, 'Person 4', 4),
-(5, 'Person 5', 5), (6, 'Person 6', 1), (7, 'Person 7', 2), (8, 'Person 8', 3), (9, 'Person 9', 4), (10, 'Person 10', 5);
+insert into company (id, name) values (1, 'Company 1'), (2, 'Company 2'), (3, 'Company 3'), (4,'Company 4'), (5,'Company 5');
+insert into person (id, name, company_id) values (1, 'Person 1', 1), (2, 'Person 2', 2), (3, 'Person 3', 3), (4, 'Person 4', 4),
+(5, 'Person 5', 5), (6, 'Person 6', 1), (7, 'Person 7', 2), (8, 'Person 8', 3), (9, 'Person 9', 4), (10, 'Person 10', 5), (11, 'Person 11', 1);
 
-SELECT p.name as person, c.name as company
-FROM person as p
-JOIN company as c
-ON p.company_id = c.id
-WHERE company_id != 5;
+insert into person (id, name, company_id) values (12, 'Person 12', 2), (13, 'Person 13', 3);
+
+select * from person where company_id != 5;
+
+select p.name as person, c.name as company
+from person as p
+join company as c
+on p.company_id = c.id;
+
+select c.name as company, count(p.id) as count_of_people
+from company as c
+join person as p
+on p.company_id = c.id
+group by c.name;
