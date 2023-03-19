e3796bdce7fcd96df453b39d1059592016cd0bc7
NikiforovaLE <n.lubovj@gmail.com>
2023-03-19 23:16:15 +0300

    Add tdd package

---
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class CinemaTest {
+
+    @Test
+    void whenBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    void whenFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+}
