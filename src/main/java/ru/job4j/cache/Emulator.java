f30d385a488bce54347ee667b81b02ea856eb5f0
NikiforovaLE <n.lubovj@gmail.com>
2023-04-09 23:50:28 +0300

    Refactor Emulator class. Improve exam_GC.md file

--- src/main/java/ru/job4j/cache/Emulator.java
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -6,10 +6,9 @@ import java.util.Scanner;
 
 public class Emulator {
     private final static String SEPARATOR = System.lineSeparator();
-    private final static List<String> menuOptions = List.of(
-            "Specify directory", "Load file content into cache", "Get file content from cache", "Exit");
+    private final static List<String> MENU_OPTIONS = List.of(
+            "1 Specify directory", "2 Load file content into cache", "3 Get file content from cache", "4 Exit");
     private final static Scanner ANSWERS_SCANNER = new Scanner(System.in);
-    private static String menuContent;
 
     public static void main(String[] args) {
         DirFileCache dirFileCache = new DirFileCache(new File(System.getProperty("user.dir") + "\\src\\main\\java\\ru\\job4j\\cache\\data").getAbsolutePath());
@@ -17,33 +16,33 @@ public class Emulator {
         emulator.init(dirFileCache);
     }
 
-    public void init(DirFileCache dirFileCache) {
+    public void init(AbstractCache<String, String> abstractCache) {
+        String menuContent = String.join(SEPARATOR, MENU_OPTIONS) + SEPARATOR;
         int select;
         boolean run = true;
-        createMenuContent();
         while (run) {
             select = askOption(menuContent);
-            if (select < 0 || select > menuOptions.size()) {
-                System.out.println("Wrong input, you can select: 0 .. " + menuOptions.size());
+            if (select < 0 || select > MENU_OPTIONS.size()) {
+                System.out.println("Wrong input, you can select: 0 .. " + MENU_OPTIONS.size());
                 continue;
             }
             switch (select) {
                 case 1:
                     System.out.print("Write directory path: ");
-                    dirFileCache = new DirFileCache(ANSWERS_SCANNER.nextLine());
+                    abstractCache = new DirFileCache(ANSWERS_SCANNER.nextLine());
                     break;
                 case 2:
                     System.out.print("Write file name: ");
                     String fileName = ANSWERS_SCANNER.nextLine();
-                    if (dirFileCache != null) {
-                       dirFileCache.load(fileName);
+                    if (abstractCache != null) {
+                        abstractCache.load(fileName);
                     }
                     break;
                 case 3:
                     System.out.print("Write file name: ");
                     String key = ANSWERS_SCANNER.nextLine();
-                    if (dirFileCache != null) {
-                        System.out.println(dirFileCache.get(key));
+                    if (abstractCache != null) {
+                        System.out.println(abstractCache.get(key));
                     }
                     break;
                 case 4:
@@ -53,19 +52,10 @@ public class Emulator {
         }
     }
 
-    private static void createMenuContent() {
-        StringBuilder menuContentBuilder = new StringBuilder("Choose an option:").append(SEPARATOR);
-        int i = 1;
-        for (String option : menuOptions) {
-            menuContentBuilder.append(i++).append(" ").append(option).append(SEPARATOR);
-        }
-        menuContent = menuContentBuilder.toString();
-    }
-
     private int askOption(String menu) {
         boolean invalid = true;
         int answer = -1;
-        do {
+        while (invalid) {
             System.out.print(menu);
             try {
                 answer = Integer.parseInt(ANSWERS_SCANNER.nextLine());
@@ -73,9 +63,7 @@ public class Emulator {
             } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again.");
             }
-        } while (invalid);
+        }
         return answer;
     }
-
-
 }
