d5f2d19b09ad3c8178fc32eff0666327dd6af1ef
NikiforovaLE <n.lubovj@gmail.com>
2023-04-10 23:49:01 +0300

    Add OCP example

---
+++ src/main/java/ru/job4j/ocp/CalculatorExample.java
@@ -0,0 +1,25 @@
+package ru.job4j.ocp;
+
+import java.util.function.BiFunction;
+
+public class CalculatorExample {
+    private static class SimpleCalculator extends AbstractCalculator<Integer> {
+        public int sum(int a, int b) {
+            return calculate(Integer::sum, a, b);
+        }
+
+        public int multiply(int a, int b) {
+            return calculate((x, y) -> x * y, a, b);
+        }
+
+        public double subtract(int a, int b) {
+            return calculate((x, y) -> x - y, a, b);
+        }
+    }
+
+    private static class AbstractCalculator<T> {
+        public T calculate(BiFunction<T, T, T> function, T first, T second) {
+            return function.apply(first, second);
+        }
+    }
+}
