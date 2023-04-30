a9e895fd2c71b6242bd8449c2e64866075410c11
NikiforovaLE <n.lubovj@gmail.com>
2023-04-30 23:45:26 +0300

    Correct ControlQualityTest class

--- src/test/java/ru/job4j/lsp/ControlQualityTest.java
+++ src/test/java/ru/job4j/lsp/ControlQualityTest.java
@@ -2,14 +2,11 @@ package ru.job4j.lsp;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.job4j.lsp.ControlQuality;
-import ru.job4j.lsp.Food;
 import ru.job4j.lsp.storage.Shop;
 import ru.job4j.lsp.storage.Trash;
 import ru.job4j.lsp.storage.Warehouse;
 
 import java.time.LocalDate;
-import java.time.Month;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,14 +19,17 @@ class ControlQualityTest {
         Warehouse warehouse = new Warehouse();
         Trash trash = new Trash();
         ControlQuality controlQuality = new ControlQuality(shop, warehouse, trash);
-        Food foodOne = new Food("one", LocalDate.of(2023, Month.APRIL, 17), LocalDate.of(2023, Month.APRIL, 4), 100.00, 80);
-        Food foodTwo = new Food("two", LocalDate.of(2023, Month.APRIL, 13), LocalDate.of(2023, Month.APRIL, 4), 100.00, 50);
-        Food foodThree = new Food("three", LocalDate.of(2023, Month.AUGUST, 27), LocalDate.of(2023, Month.APRIL, 1), 100.00, 50);
-        controlQuality.sendFoodToCorrectStorage(foodOne);
-        controlQuality.sendFoodToCorrectStorage(foodTwo);
-        controlQuality.sendFoodToCorrectStorage(foodThree);
-        assertThat(shop.showProductsInStorage()).hasSize(1).contains(foodOne);
-        assertThat(warehouse.showProductsInStorage()).hasSize(1).contains(foodThree);
-        assertThat(trash.showProductsInStorage()).hasSize(1).contains(foodTwo);
+        Food foodToWarehouse = new Food("Food One", LocalDate.now().plusDays(100), LocalDate.now().minusDays(1), 100.00, 50);
+        Food foodToShop = new Food("Food Two", LocalDate.now().plusDays(50), LocalDate.now().minusDays(50), 100.00, 50);
+        Food foodToShopWithDiscount = new Food("Food Three", LocalDate.now().plusDays(20), LocalDate.now().minusDays(80), 100.00, 80);
+        Food foodToTrash = new Food("Food Four", LocalDate.now().minusDays(1), LocalDate.now().minusDays(30), 100.00, 50);
+        controlQuality.sendFoodToCorrectStorage(foodToWarehouse);
+        controlQuality.sendFoodToCorrectStorage(foodToShop);
+        controlQuality.sendFoodToCorrectStorage(foodToShopWithDiscount);
+        controlQuality.sendFoodToCorrectStorage(foodToTrash);
+        assertThat(warehouse.showProductsInStorage()).hasSize(1).contains(foodToWarehouse);
+        assertThat(shop.showProductsInStorage()).hasSize(2).contains(foodToShop, foodToShopWithDiscount);
+        assertThat(foodToShopWithDiscount.getPrice()).isEqualTo(20);
+        assertThat(trash.showProductsInStorage()).hasSize(1).contains(foodToTrash);
     }
 }
