577c05e7b959c5848461a030a7b36ea6ac56f437
NikiforovaLE <n.lubovj@gmail.com>
2023-05-10 23:58:31 +0300

    Correct project

--- src/test/java/ru/job4j/dip/SimpleShopServiceTest.java
+++ src/test/java/ru/job4j/dip/SimpleShopServiceTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.dip;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,12 +10,21 @@ import static org.hamcrest.Matchers.*;
 
 class SimpleShopServiceTest {
 
-    private SimpleShopService shopService = new SimpleShopService();
+    private SimpleShopService shopService;
+    private User userOne;
+    private User userTwo;
+
+    @BeforeEach
+    public void fillTable() {
+        shopService = new SimpleShopService();
+        userOne = new User(1, "One");
+        userTwo = new User(2, "Two");
+    }
 
     @Test
     void whenCreateBucketTwice() {
-        assertTrue(shopService.createBucket(new User(1, "One")));
-        assertFalse(shopService.createBucket(new User(1, "One")));
+        assertFalse(shopService.createBucket(userOne));
+        assertTrue(shopService.createBucket(userOne));
     }
 
     @Test
